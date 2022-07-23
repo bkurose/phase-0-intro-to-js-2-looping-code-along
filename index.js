105fa733f483eed3698ecc39dad5c44f725007d7
Ben Kurose <ben.kurose@gmail.com>
2022-07-23 12:29:37 -0400

    commit tree eb17cb5a61067e25afa84012dce0e2d3e051e8c4

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+let greetingsStored = []
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        greetingsStored.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return greetingsStored;
+}
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log( i )
+        i -= 1
+    }
+}
+
+countDown(4)
